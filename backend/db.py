476595d7d88406208806c2fbd8c11670c6f0eff2
syuwawa <kobayashiasuka.general2@gmail.com>
2025-10-16 13:28:39 +0900

    database

---
+++ backend/db.py
@@ -0,0 +1,11 @@
+from sqlmodek import SQLModel, create_engine,Session
+
+DATABASE_URL="sqlite:///./app.db"
+engine=create_engine(DATABASE_URL,echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
